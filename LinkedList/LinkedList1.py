378f10a5039c70d22a869aa417f33fd447425694
YohanYang <john4081@naver.com>
2021-02-28 01:15:12 +0900

    LinkedList Study

--- LinkedList/LinkedList1.py
+++ LinkedList/LinkedList1.py
@@ -17,7 +17,7 @@ def add(new_data): #새로운 값을 추가함
     while node.pointer: #해당 node의 next에 값이 존재하면 그 다음 node의 주소를 해당 node로 가져온다 
         node = node.pointer # 즉 다음 노드로 계속해서 넘어가게 된다.
     node.pointer = Node(new_data) # 마지막 노드로 왔을 때는 해당 노드의 next값(다음 노드의 주소값)이 존재하지 않기 때문에 while문이 끝이난다.
-    #이 때 마지막 노드의 next 값에 새로운 선언된 노드의 주속값을 넣어준다.
+    #이 때 마지막 노드의 next 값에 새로운 선언된 노드의 주소값을 넣어준다.
 node1 = Node(1)
 head = node1
 for new_data in range(2,10):
